b4846f4466de1095d45322648078cd17064bf8bd
masayasviel <i.msya.tiger@gmail.com>
2020-06-23 22:44:01 +0900

    isNumはおかしいのでcountに変更

--- index.js
+++ index.js
@@ -1,31 +1,37 @@
 "use strict"
-const numberOfQuestions = 10;
+const numberOfQuestions = 10; // 問題数
 const alphabet = "ABCDEFGHIJKLMNOPQRSTUVWXYZ".split("");
-let str = "";
-let isNum = 0;
+const restart = document.getElementById("restart");
+const frame = document.getElementById("frame");
+let str = ""; // 問題文
+let count = 0; // 何問目
+
+const init = ()=>{
+    str = "";
+    count = 0;
+    for(let i = 0;i < numberOfQuestions;i++) str += alphabet[Math.round(Math.random()*25)];
+    document.getElementById("frame").textContent = str;
+}
+
+const main = inputKey=>{
+    if(str.charAt(0) === inputKey){
+        count++;
+        if(count < numberOfQuestions){
+            str = str.slice(1, str.length);
+            document.getElementById("frame").textContent = str;
+        }else{
+            document.getElementById("frame").textContent = "終了します\nリセットボタンを押してください";
+        }
+    }
+}
+
+restart.addEventListener("click", ()=>{
+    init();
+});
 
 window.onkeydown = e=>{
-  e.preventDefault();
-  main(e.key.toUpperCase());
+    e.preventDefault();
+    main(e.key.toUpperCase());
 }
 
-function init(){
-  str = "";
-  isNum = 0;
-  for(let i = 0;i < numberOfQuestions;i++) str += alphabet[Math.round(Math.random()*25)];
-  document.getElementById("frame").textContent = str;
-}
-
-function main(inputKey){
-  if(str.charAt(0)===inputKey){
-    isNum++;
-    if(isNum < numberOfQuestions){
-      str = str.slice(1, str.length);
-      document.getElementById("frame").textContent = str;
-    }else{
-      document.getElementById("frame").textContent = "終了します";
-    }
-  }
-}
-
-init();
+window.onload = init();
